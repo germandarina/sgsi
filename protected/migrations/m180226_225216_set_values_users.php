3add1ff562151d400c4a2f6c10aa0a01c0e63031
German Salinas <german.exequiel.salinas@gmail.com>
2018-02-26 19:58:00 -0300

    migracion para actualizar estado a los usuarios 2

--- protected/migrations/m180226_225216_set_values_users.php
+++ protected/migrations/m180226_225216_set_values_users.php
@@ -4,6 +4,7 @@ class m180226_225216_set_values_users extends CDbMigration
 {
 	public function up()
 	{
+        $this->execute("ALTER TABLE usuario CHANGE COLUMN `estado` `estado` INT(2) NULL DEFAULT '1'");
         $this->execute("UPDATE usuario set estado = 1");
 	}
 
